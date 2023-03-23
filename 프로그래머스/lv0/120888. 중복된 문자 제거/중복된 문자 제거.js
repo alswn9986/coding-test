915b18285d2c7dafa702dd293e9208743fc7c2af
Minju Han <101205543+alswn9986@users.noreply.github.com>
2023-03-23 15:58:53 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,8 +1,3 @@
 function solution(my_string) {
-    const arr = [...my_string];
-    return arr.filter((item, index) => {
-        let idx = arr.indexOf(item);
-        if (idx < index)    return false;
-        else    return true;
-    }).join('')
+    return [...new Set(my_string)].join('')
 }
