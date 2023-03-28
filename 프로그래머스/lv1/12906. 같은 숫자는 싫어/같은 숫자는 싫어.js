a90ec9afac2c966c84d4f13a9400b9dee138c9a2
Minju Han <101205543+alswn9986@users.noreply.github.com>
2023-03-28 12:41:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.58 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,12 +1,3 @@
 function solution(arr) {
-    return arr.reduce((acc, cur, idx, src) => {
-        if (idx !== 0) {
-            if (src[idx-1] !== cur) {
-                acc.push(cur)
-            }
-        } else {
-            acc.push(cur);
-        }
-        return acc;
-    }, []);
+    return arr.filter((val,index) => val != arr[index + 1]);
 }
