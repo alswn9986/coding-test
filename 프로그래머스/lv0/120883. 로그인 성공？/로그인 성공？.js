4fb320d7a1a9cc1ef4b45c40c7b312520457c46e
Minju Han <101205543+alswn9986@users.noreply.github.com>
2023-03-28 16:05:23 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,13 +1,5 @@
 function solution(id_pw, db) {
-    let [id, pw] = id_pw;
-    let checkId = db.find(x => x[0] === id);
-    if (checkId) {
-        if (checkId[1] === pw) {
-            return 'login'
-        } else {
-            return 'wrong pw'
-        }
-    } else {
-        return 'fail'
-    }
+  const [id, pw] = id_pw;
+  const map = new Map(db);
+  return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
 }
