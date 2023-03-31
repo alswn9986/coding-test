cc9dcd1af8842ebbfe160309dfb6306baa94dbd9
Minju Han <101205543+alswn9986@users.noreply.github.com>
2023-03-31 14:37:18 +0900

    [level 0] Title: 다항식 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,20 +1,16 @@
 function solution(polynomial) {
-  const arr = polynomial.split(" + ");
+    const arr = polynomial.split(" + ");
+    const xNum = arr
+                .filter(n => n.includes("x"))
+                .map(n => n.replace('x', '') || '1')
+                .reduce((acc, cur) => acc + parseInt(cur, 10), 0);
+    const num = arr
+                .filter(n => !isNaN(n))
+                .reduce((acc, cur) => acc + parseInt(cur, 10), 0);
 
-  let xNum = 0;
-  let num = 0;
-    arr.forEach((n) => {
-    if (n.includes("x")) {
-      const strVal = n.replace("x", "") || "1";
-      xNum += parseInt(strVal, 10);
-    } else {
-      num += parseInt(n);
-    }
-  });
+    let answer = [];
+    if(xNum) answer.push(`${xNum === 1 ? "" : xNum}x`);
+    if(num) answer.push(num);
 
-  let answer = [];
-  if (xNum) answer.push(`${xNum === 1 ? "" : xNum}x`);
-  if (num) answer.push(num);
-    
-  return answer.join(" + ");
+    return answer.join(" + ");
 }
