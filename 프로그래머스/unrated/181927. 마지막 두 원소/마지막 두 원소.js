e01eaa7c3c4de200a4ff8de56b2e77a6370583bc
Minju Han <101205543+alswn9986@users.noreply.github.com>
2023-06-15 10:56:55 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,11 +1,4 @@
 function solution(num_list) {
-    const idx = num_list.length - 1;
-    const last = num_list[idx];
-    const secondToLast = num_list[idx - 1];
-    if (last > secondToLast) {
-        num_list.push(last - secondToLast);
-    } else {
-        num_list.push(last * 2);
-    }
-    return num_list;
+    const [a, b] = [...num_list].reverse();
+    return [...num_list, a > b ? (a-b) : a*2];
 }
