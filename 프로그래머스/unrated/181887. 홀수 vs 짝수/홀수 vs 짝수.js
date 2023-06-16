2073505ef17aeef8833e2f5abfed7acc781c3684
Minju Han <101205543+alswn9986@users.noreply.github.com>
2023-06-16 10:24:50 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,7 +1,4 @@
 function solution(num_list) {
-    let [even, odd] = num_list.reduce((acc, curr, idx) => {
-        acc[(idx + 1) % 2] = acc[(idx + 1) % 2] + curr;
-        return acc;
-    }, [0, 0]);
+    let [even, odd] = num_list.reduce(([a, b], curr, idx) => idx % 2 ? [a + curr, b] : [a, b + curr], [0, 0]);
     return Math.max(even, odd);
 }
