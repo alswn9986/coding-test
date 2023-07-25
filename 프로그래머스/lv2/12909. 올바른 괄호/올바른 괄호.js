67a990248d8075777024c63046996bb394f5d0f4
Minju Han <101205543+alswn9986@users.noreply.github.com>
2023-07-25 15:26:33 +0900

    [level 2] Title: 올바른 괄호, Time: 2.35 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -6,20 +6,5 @@ function solution(s) {
             return false;
         }
     }
-    const peek = (arr) => arr[arr.length - 1];
-    const arr = [];
-    for (let i = 0; i < s.length; i++) {
-        const c = s[i];
-        if (c === ')') {
-            const prev = peek(arr);
-            if (prev) {
-                arr.pop();
-            } else {
-                return false;
-            }
-        } else {
-            arr.push(true);
-        }
-    }
-    return arr.length === 0 ? true : false;
+    return cnt === 0 ? true : false;
 }
